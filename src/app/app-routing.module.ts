44ea3e7e19f4ba8539629ca7c0be18224546e41d
Fernando Soares <fndcaique@gmail.com>
2022-08-30 20:00:31 -0300

    add login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/albums' },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent },
   {
     path: 'albums',
     loadChildren: () => import('./albums/albums.module')
