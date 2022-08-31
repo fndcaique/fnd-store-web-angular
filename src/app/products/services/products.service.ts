c71b93f8aa785886753e41b2cf2a83f639d848f6
Fernando Soares <fndcaique@gmail.com>
2022-08-30 23:41:27 -0300

    add products service

---
+++ src/app/products/services/products.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { delay, take, tap } from 'rxjs';
+import { Product } from '../model/product';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  private readonly API = '/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  findAll() {
+    return this.httpClient.get<Product[]>(this.API)
+      .pipe(
+        // first(),
+        take(1),
+        delay(2000),
+        tap(products => console.log(products))
+      );
+  }
+}
