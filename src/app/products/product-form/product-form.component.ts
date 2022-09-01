85a44f0dcea19455805ec72beed58bb5693e719a
Fernando Soares <fndcaique@gmail.com>
2022-09-01 15:42:48 -0300

    add on error to product-form

--- src/app/products/product-form/product-form.component.ts
+++ src/app/products/product-form/product-form.component.ts
@@ -1,5 +1,5 @@
 import { Location } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NonNullableFormBuilder } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
@@ -11,7 +11,7 @@ import { ProductsService } from './../services/products.service';
   templateUrl: './product-form.component.html',
   styleUrls: ['./product-form.component.scss']
 })
-export class ProductFormComponent implements OnInit {
+export class ProductFormComponent {
 
   form = this.formBuilder.group({
     name: [''],
@@ -25,21 +25,23 @@ export class ProductFormComponent implements OnInit {
     private location: Location,
   ) { }
 
-  ngOnInit(): void { }
-
   onSubmit() {
     this.service.create(this.form.value as Product).subscribe({
       next: result => this.location.back(),
-      error: error => {
-        this.dialog.open(ErrorDialogComponent, { data: 'Error on create product' });
-      },
+      error: error =>
+        this.onError('Error on create product'),
       complete: () => console.log('completed'),
-
     });
   }
 
   onCancel() {
     this.location.back();
+  }
+
+  onError(errorMessage: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMessage
+    })
   }
 
 }
